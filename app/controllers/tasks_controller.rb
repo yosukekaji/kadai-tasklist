7bbeeb204b801ccefd174def29e89945dcc5696a
yosuke.kaji <kaji@flick-inc.co.jp>
2019-09-16 12:49:14 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Task は正常に入力されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は入力されませんでした'
+      render :new
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に入力されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は入力されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def message_params
+    params_require(:task).permit(:content)
+  end
+  
+end
